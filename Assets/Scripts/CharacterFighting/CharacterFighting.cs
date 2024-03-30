10ce67cac3a32d24da188fa5b3a65754cf443a36
DESKTOP-N4F36O6\roylo <roylouis0424@gmail.com>
2024-03-30 15:03:35 +0100

    Health

--- Assets/Scripts/CharacterFighting/CharacterFighting.cs
+++ Assets/Scripts/CharacterFighting/CharacterFighting.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class CharacterFighting : MonoBehaviour
 {
+
+    public KeyCode PunchAttack01;
+    public KeyCode KickAttack01;
     public GameObject Character;
     Animator controllerANIM;
 
@@ -18,10 +21,16 @@ public class CharacterFighting : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetKeyDown("A"))
+        if (Input.GetKeyDown(PunchAttack01))
         {
             attack1();
         }
+        if (Input.GetKeyDown(KickAttack01))
+        {
+            attack2();
+        }
+
+        // faire la memem chose si j'arrive pas a rednre le bouclier automatique
     }
 
     [ContextMenu("Attack01")]
@@ -29,10 +38,28 @@ public class CharacterFighting : MonoBehaviour
     {
         
         controllerANIM.SetTrigger("Punching01");
+        isBlocking = false;
+        controllerANIM.SetBool("Blocking", isBlocking);
+    }
+
+    [ContextMenu("Kicking01")]
+    public void attack2()
+    {
+        controllerANIM.SetTrigger("Kicking01");
+        isBlocking = false;
+        controllerANIM.SetBool("Blocking", isBlocking);
     }
 
     public void SetBlocking()
     {
-
+        if(!isBlocking)
+        {
+            isBlocking = false;
+        }
+        else if (!isBlocking)
+        {
+            isBlocking = true;
+        }
+        controllerANIM.SetBool("Blocking", isBlocking);
     }
 }
---
+++ Assets/Scripts/PlayerHealth/PlayerHealth.cs
@@ -0,0 +1,98 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEditor.Overlays;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour
+{
+
+    private float health;
+    private float lerpTimer;
+    [Header("Health Bar")]
+    public float maxHealth = 100;
+    public float chipSpeed = 2f;
+    public Image frontHealthBar;
+    public Image backHealthBar;
+    public TextMeshProUGUI healthText;
+
+    //[Header(" Damage Overlay")]
+    //public Image DamageOverlay;
+    //public float duration;
+    //public float fadeSpeed;
+
+    //private float durationTimer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = maxHealth;
+        //DamageOverlay.color = new Color(DamageOverlay.color.r, DamageOverlay.color.g, DamageOverlay.color.b, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        health = Mathf.Clamp(health, 0, maxHealth);
+        UpdateHealthUI();
+        //if (DamageOverlay.color.a > 0)
+        //{
+        //    if (health < 30)
+        //    {
+        //        return;
+        //    }
+        //    durationTimer += Time.deltaTime;
+        //    if (durationTimer > duration)
+        //    {
+        //        float tempAlpha = DamageOverlay.color.a;
+        //        tempAlpha -= Time.deltaTime * fadeSpeed;
+        //        DamageOverlay.color = new Color(DamageOverlay.color.r, DamageOverlay.color.g, DamageOverlay.color.b, tempAlpha);
+        //    }
+        //}
+
+
+    }
+    public void UpdateHealthUI()
+    {
+        Debug.Log(health);
+        float fillF = frontHealthBar.fillAmount;
+        float fillB = backHealthBar.fillAmount;
+        float hFraction = health / maxHealth;
+        if (fillB > hFraction)
+        {
+            frontHealthBar.fillAmount = hFraction;
+            backHealthBar.color = Color.red;
+            lerpTimer += Time.deltaTime;
+            float percentComplete = lerpTimer / chipSpeed;
+            percentComplete = percentComplete * percentComplete;
+            backHealthBar.fillAmount = Mathf.Lerp(fillB, hFraction, percentComplete);
+        }
+        if (fillF < hFraction)
+        {
+
+            backHealthBar.color = Color.green;
+            backHealthBar.fillAmount = hFraction;
+            lerpTimer += Time.deltaTime;
+            float percentComplete = lerpTimer / chipSpeed;
+            percentComplete = percentComplete * percentComplete;
+            frontHealthBar.fillAmount = Mathf.Lerp(fillF, backHealthBar.fillAmount, percentComplete);
+        }
+    }
+
+    public void TakeDamage(float damage)
+    {
+        health -= damage;
+        healthText.text = health.ToString();
+        lerpTimer = 0f;
+        //durationTimer = 0;
+        //DamageOverlay.color = new Color(DamageOverlay.color.r, DamageOverlay.color.g, DamageOverlay.color.b, (float)0.3);
+    }
+    public void RestoreHealth(float healAmount)
+    {
+        health += healAmount;
+        healthText.text = health.ToString();
+        lerpTimer = 0f;
+    }
+
+}
